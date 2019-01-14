999d5b7ee2cd445769e13b78204173e229a6c27b
adeel982010 <adeel982010@gmail.com>
2019-01-14 16:09:02 +0500

    Commit file

---
+++ scrap_type/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ScrapType",
+
+    'summary': """ Scrap management""",
+
+    'description': """
+Scrap type is a module to manage Scrap    """,
+
+    'author': "Axiom",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'stock'],
+
+    # always loaded
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+        'security/ir.model.access.csv',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
